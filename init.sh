273faa272a532423c61d8a7ffef25370aa951794
jhtwosun <jhtwosun@gmail.com>
2025-07-19 18:21:37 +0900

    .

--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
-python3 -m virtualenv .dlr
+cd ..
 
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10 python3.10-dev -y
+
+python3 -m pip install --user -U virtualenv
+
+PATH=$PATH:~/.local/bin
+python3 -m virtualenv .dlr --python=python3.10
+PATH=$PATH:~/.dlr/bin
 source .dlr/bin/activate
 
-cd dictionary_learning_demo
+cd sae
 
 git config submodule.dictionary_learning.url https://github.com/saprmarks/dictionary_learning.git
 
@@ -11,4 +20,5 @@ pip install -e .
 
 env HF_HOME='/mnt/tmp/hf_cache'
 echo HF_HOME='/mnt/tmp/hf_cache' >> ~/.bashrc
-source ~/.bashrc
+
+sudo apt install tmux -y
