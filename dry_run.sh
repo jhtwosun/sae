00084c2d88941f54a8c7690c4a4a3e203b8e09a8
jhtwosun <jhtwosun@gmail.com>
2025-07-19 08:37:10 +0000

    .

--- dry_run.sh
+++ dry_run.sh
@@ -1,1 +1,2 @@
+export HF_HOME=/mnt/tmp/hf_cache
 python demo.py --dry_run --save_dir /mnt/tmp/dlr_log/run2 --model_name EleutherAI/pythia-70m-deduped --layers 3 --architectures standard jump_relu batch_top_k top_k gated
--- parallel_run.sh
+++ parallel_run.sh
@@ -1,1 +1,3 @@
+export HF_HOME=/mnt/tmp/hf_cache
 
+python parallel_training.py 
